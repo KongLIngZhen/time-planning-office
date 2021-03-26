df8bc480f0ba7c99aa39cf449af9348502bce5b5
孔令振 <konglingzhen@ruyucapital.com>
2021-03-26 23:13:00 +0800

    feat: 测试提交

--- dist/pages/mine/index.js
+++ dist/pages/mine/index.js
@@ -62,7 +62,7 @@ var Index = /*#__PURE__*/function (_Component) {
       return /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__["jsx"])(_tarojs_components__WEBPACK_IMPORTED_MODULE_5__[/* View */ "b"], {
         className: "index",
         children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__["jsx"])(_tarojs_components__WEBPACK_IMPORTED_MODULE_5__[/* Text */ "a"], {
-          children: "\u6211\u7684\u5C0F\u7A0B\u5E8F"
+          children: "\u6211\u7684\u5C0F\u7A0B\u5E8F "
         })
       });
     }
--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -17,7 +17,7 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>我的小程序</Text>
+        <Text>我的小程序 </Text>
       </View>
     )
   }
